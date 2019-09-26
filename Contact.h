dc6b64e1d52790230601804b10747e1d16cd774b
Chuanping ZHUANG <zcphoenix@outlook.com>
2019-09-26 15:39:30 +0800

    Contact中去除密码字段

--- Contact.h
+++ Contact.h
@@ -4,13 +4,12 @@
 
 class Contact {
 public:
-    Contact(unsigned int account, const char* nickname, const char* password)
-        : m_account(account), m_nickname(nickname), m_password(password)
+    Contact(unsigned long long account, const char* nickname)
+        : m_account(account), m_nickname(nickname)
     {
     }
 
 public:
-    unsigned int m_account = 0;
+    unsigned long long m_account = 0;
     std::string m_nickname = "";
-    std::string m_password = "";
 };
