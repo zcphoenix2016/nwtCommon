3002529fcf08ee50f3a31ba419d1f424a1714a31
Chuanping ZHUANG <zcphoenix@outlook.com>
2019-09-27 16:32:59 +0800

    定义登录消息体

--- Commands.h
+++ Commands.h
@@ -1,5 +1,39 @@
 #pragma once
 
-const unsigned int CMD_LOGIN = 0x1000;
+#include "NwtHeader.h"
 
-const unsigned int CMD_INSTANT_MSG = 0x2000;
+const unsigned int CMD_LOGIN_REQ = 0x1000;
+const unsigned int CMD_LOGIN_RSP = 0x4000;
+const unsigned int CMD_INSTANT_MSG = 0x1001;
+
+class BaseMsg {
+public:
+    BaseMsg() {
+        m_head = NwtHeader();
+    }
+
+public:
+    NwtHeader m_head;
+};
+
+class LoginReq : public BaseMsg {
+public:
+    LoginReq() {
+        BaseMsg();
+    }
+
+public:
+    unsigned int m_account = 0;
+    char m_password[32] = { 0 };
+};
+
+class LoginRsp : public BaseMsg {
+public:
+    LoginRsp() {
+        BaseMsg();
+    }
+
+public:
+    unsigned int m_account = 0;
+    unsigned int m_result = 0;
+};
--- NwtHeader.h
+++ NwtHeader.h
@@ -3,14 +3,16 @@
 class NwtHeader
 {
 public:
+    NwtHeader() {
+    }
+
     NwtHeader(unsigned int cmd, unsigned int srcAccount, unsigned int tarAccount, unsigned int contentLength)
-        : m_cmd(cmd), m_srcAccount(srcAccount), m_tarAccount(tarAccount), m_contentLength(contentLength)
-    {
+        : m_cmd(cmd), m_srcAccount(srcAccount), m_tarAccount(tarAccount), m_contentLength(contentLength) {
     }
 
 public:
     unsigned int m_cmd = 0;
-    unsigned int m_srcAccount = 0;
+    unsigned int m_srcAccount = 0; //TODO: refactor to char arrary according to Contact definition ???
     unsigned int m_tarAccount = 0;
     unsigned int m_contentLength = 0;
 };
